4d881a1ad7d2a21a1af32118c9b5ca9207897114
nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2025-06-11 17:14:44 -0300

    a

--- src/app/resumeCandidate/page.tsx
+++ src/app/resumeCandidate/page.tsx
@@ -1,7 +1,14 @@
+"use client"
+
 import { HeaderLoggedAdmin } from "@/components/headerAdmin/page"
 import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
 import { GoBack } from "@/components/goBack/page";
-import { Divider, Link } from "@mui/material";
+import { Button, Divider, Link } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkIcon from '@mui/icons-material/Link';
 
 const ResumeCandidate = () => {
 
@@ -22,6 +29,34 @@ const ResumeCandidate = () => {
         Advanced = 2,
         Fluent = 3
     }
+
+    enum EApplicationStatus {
+        ANALISE = 0,
+        APROVADO = 1,
+        REPROVADO = 2,
+        DINAMICA = 3,
+        ENTREVISTA = 4,
+    }
+
+    interface IApplication {
+        
+        id : number,
+        candidate : {
+            id : number,
+            name : string,
+            email : string,
+            phone : string | null
+        },
+        status : EApplicationStatus,
+        note : string,
+        dismissalStage : {
+            id : number,
+            description : string,
+            startDate : Date,
+            endDate : Date
+        } | null
+    }[]
+     
 
     interface IResume {
         
@@ -144,21 +179,61 @@ const ResumeCandidate = () => {
             ],
             "links" : [{
                 "id" : 1,
-                "url" : "https://github.com/nickyzz1",
+                "url" : "https://github.com/sasa2754",
                 "description" : "Github"
-            }]
+            },
+            {
+                "id" : 2,
+                "url" : "https://instagram.com/Sabrina.2754",
+                "description" : "Instagram"
+            },
+            {
+                "id" : 3,
+                "url" : "https://br.linkedin.com/in/sabrina-mortean-70a52b230",
+                "description" : "Linkedin"
+            },
+            {
+                "id" : 4,
+                "url" : "https://sabrinaportifolio.com",
+                "description" : "Portifólio"
+            },
+            {
+                "id" : 5,
+                "url" : "https://projeto.com",
+                "description" : "Projeto React"
+            },
+        ]
         }
 
+        const application: IApplication = {
+            id: 1,
+            candidate: {
+            id: 1,
+            name: "Sabrina Mortean Loures de Souza",
+            email: "Sabrina.souza.2754@gmail.com",
+            phone: "41 99999-9999"
+            },
+            status: 1,
+            note: `Sou Sabrina, desenvolvedora com experiência sólida como desenvolvedora plena e também como Product Owner. Essa trajetória me proporcionou uma visão ampla do ciclo completo de desenvolvimento, desde o entendimento das necessidades do negócio até a entrega de soluções técnicas eficientes. 
+
+            Após atuar como PO, desenvolvi habilidades valiosas de comunicação, priorização e alinhamento com stakeholders, que agregam muito no papel de uma desenvolvedora sênior, que deve liderar tecnicamente e guiar o time.
+
+            Tenho grande motivação para retornar à prática do desenvolvimento, aplicando tanto meu conhecimento técnico quanto minha experiência de gestão para contribuir de forma estratégica e inovadora na Darede, especialmente nos desafios da nuvem.
+
+            Acredito que essa vaga de Desenvolvedora Sênior é o próximo passo ideal para minha carreira, pois me permitirá crescer tecnicamente, liderar projetos mais complexos e atuar com impacto direto no sucesso da empresa.`,
+            dismissalStage: null
+        }
+        
         const dataVacancy = {
             name : "Desenvolvedor React Sênior"
         }
 
-    const levels: { [key: number]: string } = {
-    [EProficiencyLevel.Benniger]: "Básico",
-    [EProficiencyLevel.Intermediate]: "Intermediário",
-    [EProficiencyLevel.Advanced]: "Avançado",
-    [EProficiencyLevel.Fluent]: "Fluente",
-    };
+        const levels: { [key: number]: string } = {
+        [EProficiencyLevel.Benniger]: "Básico",
+        [EProficiencyLevel.Intermediate]: "Intermediário",
+        [EProficiencyLevel.Advanced]: "Avançado",
+        [EProficiencyLevel.Fluent]: "Fluente",
+        };
 
 
         return(
@@ -199,9 +274,10 @@ const ResumeCandidate = () => {
                                 </div>
                                 <div>
                                     {data.links.map((i, index) => {
+                                        if(i.url.includes("linkedin"))
                                         return(
                                             <div key={index} className="flex-wrap" >
-                                                <h2 className="text-neutral-400">{i.description}</h2>
+                                                <h2 className="text-neutral-400">Linkedin</h2>
                                                 <Link href="#" variant="body2">
                                                 {i.url}
                                                 </Link>
@@ -264,7 +340,7 @@ const ResumeCandidate = () => {
                                 {
                                     data.skills.map((s, index) => {
                                         return(
-                                            <p className="border rounded-2xl bg-green-100 px-2 border-green-700">{s.name}</p>      
+                                            <p key={index} className="border rounded-2xl bg-green-100 px-2 border-green-700">{s.name}</p>      
                                         )
                                     })
                                 }
@@ -294,15 +370,41 @@ const ResumeCandidate = () => {
                                 {
                                     data.links.map((l, index) => {
                                         return(
-                                            <div>
-                                            <Link href="#" variant="body2">
-                                                {l.url}
-                                            </Link>
-
+                                            <div key={index} className="flex gap-2">
+                                                {l.url.includes("github") ? (
+                                                    <Button color="success" onClick={() => window.open(l.url, '_blank', 'noopener,noreferrer')}>
+                                                        <GitHubIcon/>
+                                                    </Button>
+                                                ) :  l.url.includes("instagram") ? (
+                                                    <Button color="success" onClick={() => window.open(l.url, '_blank', 'noopener,noreferrer')}>
+                                                        <InstagramIcon/>
+                                                    </Button>
+                                                ) : l.url.includes("linkedin") ? (
+                                                    <Button color="success" onClick={() => window.open(l.url, '_blank', 'noopener,noreferrer')}>
+                                                        <LinkedInIcon/>
+                                                    </Button>
+                                                ) : l.url.includes("facebook") ? (
+                                                    <Button color="success" onClick={() => window.open(l.url, '_blank', 'noopener,noreferrer')}>
+                                                        <FacebookIcon/>
+                                                    </Button>
+                                                ) : (
+                                                    <Button className="flex gap-4" color="success" onClick={() => window.open(l.url, '_blank', 'noopener,noreferrer')}>
+                                                        <LinkIcon/>
+                                                        <p>{l.description}</p>
+                                                    </Button>
+                                                )}
                                             </div>
                                         )
                                     })
                                 }
+                                </div>
+                            </div>
+                            {/* Nota para recrutador */}
+                            <div className="flex flex-col gap-6" >
+                                <h1 className="text-[#036d3c] text-xl font-semibold" >Nota para recrutador</h1>
+                                <Divider/>
+                                <div className="flex gap-2 flex-wrap wrap px-2">
+                                    <p>{application.note}</p>
                                 </div>
                             </div>
 
